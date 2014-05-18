a3032bea56b534781e5c38f0b78a5b261aeaa76d
Giovanni Maggini <giovanni.maggini@gmail.com>
2014-05-18 18:44:57 +0200

    improved pick list values retrieval for activities

--- Helpers/CRMPicklistHelper.m
+++ Helpers/CRMPicklistHelper.m
@@ -8,12 +8,43 @@
 
 #import "CRMPicklistHelper.h"
 #import "CRMConstants.h"
+#import "CRMFieldConstants.h"
 #import "GWActivitySelectItem.h"
+#import "CRMFieldPicklistValue.h"
 
 @implementation CRMPicklistHelper
 
 + (NSArray*)picklistValuesForActivityStatus:(NSString*)activityType
 {
+    NSPredicate *p = [NSPredicate predicateWithFormat:@"service = %@ AND crm_name = %@", [Service getActive], kVTModuleCalendar];
+    Module *crmmodule = [Module MR_findFirstWithPredicate:p];
+    
+    if ([activityType isEqualToString:kCRMActivityTypeTask]) {
+        //We need to load the picklist values for the field "taskstatus"
+
+        CRMField *field = [CRMField MR_findFirstWithPredicate:[NSPredicate predicateWithFormat:@"module = %@ AND crm_name = %@", crmmodule, kCalendarFieldtaskstatus]];
+        if (field != nil) {
+            NSMutableArray *values = [[NSMutableArray alloc] init];
+            for (CRMFieldPicklistValue *picklistValue in field.picklist_values) {
+                [values addObject:[GWActivitySelectItem itemWithLabel:picklistValue.crm_label value:picklistValue.crm_value]];
+            }
+            return values;
+        }
+    }
+    else if([activityType isEqualToString:kCRMActivityTypeMeeting] || [activityType isEqualToString:kCRMActivityTypeCall]){
+        //We need to load the picklist values for the "eventstatus" field
+        CRMField *field = [CRMField MR_findFirstWithPredicate:[NSPredicate predicateWithFormat:@"module = %@ AND crm_name = %@", crmmodule, kCalendarFieldeventstatus]];
+        if (field != nil) {
+            NSMutableArray *values = [[NSMutableArray alloc] init];
+            for (CRMFieldPicklistValue *picklistValue in field.picklist_values) {
+                [values addObject:[GWActivitySelectItem itemWithLabel:picklistValue.crm_label value:picklistValue.crm_value]];
+            }
+            return values;
+        }
+    }
+    
+    
+    //This is now just a "fallback" and we expect it should be called only in the unlucky event that CRMField is nil
     if ([activityType isEqualToString:kCRMActivityTypeTask]) {
         return @[[GWActivitySelectItem itemWithLabel:NSLocalizedString(kCRMActivityTaskStatusCompleted, @"Taks Status Completed") value:kCRMActivityTaskStatusCompleted],
                  [GWActivitySelectItem itemWithLabel:NSLocalizedString(kCRMActivityTaskStatusDeferred, @"Task Status Deferred") value:kCRMActivityTaskStatusDeferred],
@@ -23,11 +54,14 @@
                  [GWActivitySelectItem itemWithLabel:NSLocalizedString(kCRMActivityTaskStatusPlanned, @"Task Status Planned") value:kCRMActivityTaskStatusPlanned]
                  ];
     }
-    if ([activityType isEqualToString:kCRMActivityTypeMeeting] || [activityType isEqualToString:kCRMActivityTypeCall]) {
+    else if ([activityType isEqualToString:kCRMActivityTypeMeeting] || [activityType isEqualToString:kCRMActivityTypeCall]) {
         return
-        @[[GWActivitySelectItem itemWithLabel:NSLocalizedString(kCRMActivityCallMeetingStatusHeld, @"Call Meeting Status Held") value:kCRMActivityCallMeetingStatusHeld],
-          [GWActivitySelectItem itemWithLabel:NSLocalizedString(kCRMActivityCallMeetingStatusNotHeld, @"Call Meeting Status Not Held") value:kCRMActivityCallMeetingStatusNotHeld],
-          [GWActivitySelectItem itemWithLabel:NSLocalizedString(kCRMActivityCallMeetingStatusPlanned, @"Call Meeting Status Planned") value:kCRMActivityCallMeetingStatusPlanned]
+        @[[GWActivitySelectItem itemWithLabel:NSLocalizedString(([NSString stringWithFormat:@"%@",kCRMActivityCallMeetingStatusHeld]), @"Call Meeting Status Held")
+                                        value:kCRMActivityCallMeetingStatusHeld],
+          [GWActivitySelectItem itemWithLabel:NSLocalizedString(kCRMActivityCallMeetingStatusNotHeld, @"Call Meeting Status Not Held")
+                                        value:kCRMActivityCallMeetingStatusNotHeld],
+          [GWActivitySelectItem itemWithLabel:NSLocalizedString(kCRMActivityCallMeetingStatusPlanned, @"Call Meeting Status Planned")
+                                        value:kCRMActivityCallMeetingStatusPlanned]
           ];
     }
     else return @[];
