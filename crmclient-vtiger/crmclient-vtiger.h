d6413d7ae9a00cc7a17a19cd5fe2488d62b47efd
Giovanni Maggini <giovanni.maggini@gmail.com>
2014-09-16 21:43:26 +0200

    reorganised files and created a Framework Xcode project

---
+++ crmclient-vtiger/crmclient-vtiger.h
@@ -0,0 +1,19 @@
+//
+//  crmclient-vtiger.h
+//  crmclient-vtiger
+//
+//  Created by Giovanni on 16/09/14.
+//  Copyright (c) 2014 gixWorks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for crmclient-vtiger.
+FOUNDATION_EXPORT double crmclient_vtigerVersionNumber;
+
+//! Project version string for crmclient-vtiger.
+FOUNDATION_EXPORT const unsigned char crmclient_vtigerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <crmclient_vtiger/PublicHeader.h>
+
+
